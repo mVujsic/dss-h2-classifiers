9adb334d4c513abdbddf32068f836469111bd8fd
Mateja <mateja.vujsic@gmail.com>
2022-10-27 20:51:55 +0200

    Code changes

--- classifiers/logistic_regression.py
+++ classifiers/logistic_regression.py
@@ -3,7 +3,7 @@ from sklearn import linear_model
 
 def logistic_regression(X_train, X_test, y_train, y_test):
     print("Logistic regression: solver=liblinear, max_iter=200, class_weight={0:0.8, 1:1}, C=0.7")
-    model = linear_model.LogisticRegression(class_weight={0:0.8, 1:1}, C=0.7,
+    model = linear_model.LogisticRegression(class_weight={0: 0.8, 1: 1}, C=0.7,
                                             solver='liblinear',
                                             max_iter=200)
     model.fit(X_train, y_train)
